f9217c245543d515de789506227ae67d81a6c6fe
DESKTOP-EPN9CP6\thebe <oleh.knyaz@gmail.com>
2019-02-26 20:23:19 +0200

    add structure

---
+++ MvcIdentityBank/Models/DbModels/Account.cs
@@ -0,0 +1,15 @@
+﻿namespace MvcIdentityBank
+{
+    public class Account
+    {
+        public int Id { get; set; }
+
+        public string Number { get; set; }
+        public decimal Qty { get; set; }
+
+        //navigation poperty to CustomUser
+        public virtual CustomUser CustomUser { get; set; }
+
+
+    }
+}
---
+++ MvcIdentityBank/Models/DbModels/CustomUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+
+namespace MvcIdentityBank
+{
+    public class CustomUser: IdentityUser 
+    {
+
+        public string SkinColor { get; set; }
+        public DateTime LastVisit { get; set; }
+
+        //navigation property
+        public virtual ICollection<Account> Accounts { get; set; }
+    }
+}
