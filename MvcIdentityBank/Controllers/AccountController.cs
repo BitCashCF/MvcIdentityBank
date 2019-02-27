f33339c1d59813f397dc3e906deefd424c4aff0a
DESKTOP-EPN9CP6\thebe <oleh.knyaz@gmail.com>
2019-02-27 21:12:45 +0200

    Add all changes

--- MvcIdentityBank/Controllers/AccountController.cs
+++ MvcIdentityBank/Controllers/AccountController.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNet.Identity.Owin;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
+using MvcIdentityBank.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -25,9 +28,96 @@ namespace MvcIdentityBank.Controllers
         [HttpGet]
         public ActionResult Register()
         {
-            return View()
+            return View();
         }
 
+        [HttpPost]
+        public async Task<ActionResult> Register(RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Create user without adding to DB
+                CustomUser customUser = new CustomUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    SkinColor = model.SkinColor
+                };
+                //create UserWithIdentity from simple User
+                var result = await UserManager.CreateAsync(
+                    customUser,model.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    foreach (var item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item);
+                    }
+                    //result.Errors
+                    //    .ToList().ForEach(i =>{ModelState.AddModelError("", i);});
+                }
+            }
+            return View(model);
+        }
 
+        [HttpGet]
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.returnUrl = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Login(LoginModel model,string returnUrl)
+        {
+            ActionResult returnResult = null;
+            if (ModelState.IsValid)
+            {
+                var customUser = await UserManager.FindAsync(model.Email, model.Password);
+                if (customUser == null)
+                {
+                    ModelState.AddModelError("", "Password or Login is INVALID!!!");
+                }
+                else
+                {
+                    var result = await UserManager.CreateIdentityAsync(customUser,
+                        DefaultAuthenticationTypes.ApplicationCookie);
+                    AuthManager.SignOut();
+                    AuthManager.SignIn(new AuthenticationProperties
+                    {
+                        IsPersistent = true
+                    }, result);
+
+                    if (String.IsNullOrEmpty(returnUrl))
+                    {
+                        returnResult = RedirectToAction("Index", "Home");
+                        //return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        returnResult = Redirect(returnUrl);
+                        //return Redirect(returnUrl);
+                    }
+                }
+            }
+            else
+            {
+                ViewBag.returnUrl = returnUrl;
+                returnResult = View(model);
+            }
+            //return View(model);
+            return returnResult;
+        }
+
+        [HttpGet]
+        public ActionResult Logout()
+        {
+            AuthManager.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
--- MvcIdentityBank/Models/ViewModels/RegisterModel.cs
+++ MvcIdentityBank/Models/ViewModels/RegisterModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,19 @@ namespace MvcIdentityBank.Models.ViewModels
 {
     public class RegisterModel
     {
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords are not the same!!!")]
+        public string PasswordConfirm { get; set; }
+
+        [Required]
+        public string SkinColor { get; set; }
     }
 }
